51dc213b1a768fef501151d8cebb1eefdb3c1ae8
John Cirincione <jtcirinc@ncsu.edu>
2025-02-23 18:00:22 -0500

    v2.0.0

---
+++ ChessImproved/src/core/main.py
@@ -0,0 +1,19 @@
+import pygame as p
+
+
+# Size of board
+WIDTH = HEIGHT = 512
+
+# menu size
+MENU_WIDTH = MENU_HEIGHT = 200
+
+# Play again button dimensions
+BUTTON_HEIGHT = 25
+BUTTON_WIDTH = 100
+# Dimensions of the board
+DIMENSION = ROWS = COLS = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
